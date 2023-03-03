143879688cd397f81c49f3172426f6cfc406c318
Godlikefreq <godlikefreq@gmail.com>
2023-03-03 23:43:07 +0300

    Fixing user-online appearance

--- app/channels/online_channel.rb
+++ app/channels/online_channel.rb
@@ -1,6 +1,5 @@
 class OnlineChannel < Turbo::StreamsChannel
   after_unsubscribe :handle_offline
-  after_subscribe :handle_online
 
   def subscribed
     super
@@ -8,6 +7,12 @@ class OnlineChannel < Turbo::StreamsChannel
     stream_for current_user
     users_online = Kredis.unique_list "users_online"
     users_online << current_user.id
+    Turbo::StreamsChannel.broadcast_append_to(
+      verified_stream_name_from_params,
+      target: "users-list",
+      partial: "users/user",
+      locals: { user: current_user }
+    )
   end
 
   def unsubscribed
@@ -17,14 +22,5 @@ class OnlineChannel < Turbo::StreamsChannel
 
   def handle_offline
     HandleOfflineJob.set(wait_until: Time.zone.now + 5).perform_later(current_user)
-  end
-
-  def handle_online
-    Turbo::StreamsChannel.broadcast_append_to(
-      verified_stream_name_from_params,
-      target: "users-list",
-      partial: "users/user",
-      locals: { user: current_user }
-    )
   end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,8 +5,7 @@ class RoomsController < ApplicationController
     @rooms = Room.all
     @room = Room.new
 
-    users_online = Kredis.unique_list "users_online"
-    @users = User.find(users_online.elements)
+    @users = User.find((Kredis.unique_list "users_online").elements)
   end
 
   def show

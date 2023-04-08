0f1161b13684e2b96c3f9512f243dffe27a35456
Godlikefreq <godlikefreq@gmail.com>
2023-04-08 13:41:55 +0300

    Added color schemes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
+  get "set_theme", to: "theme#update"
 
   resources :rooms, only: %i[ create index show ], param: :token do
     resources :messages, only: %i[ create show ]
